217ac2e97cbc2c2a3b4df8d32da9f4fb8f22c334
p-carpenter <poppy.carpenter@ada.ac.uk>
2025-03-05 15:27:53 +0000

    feat: added sentry for test debugging

---
+++ sentry.server.config.js
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7d9411e18e29e396284b3a47c2e85509@o4508925629497349.ingest.de.sentry.io/4508925633757264",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
