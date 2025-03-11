bfbbcf3437a6b1708d151b5db45fad84216d6fd3
p-carpenter <poppy.carpenter@ada.ac.uk>
2025-03-11 02:28:45 +0000

    tests: forgot to add to previous commit - added polyfill for Object.defineProperty, Response, Request, Headers & ResizeObserver, mocked env variables for tests, configured MSW instead of mocking fetch

--- jest.setup.js
+++ jest.setup.js
@@ -1,14 +1,55 @@
-import '@testing-library/jest-dom';
-import { setupServer } from 'msw/node';
-import { handlers } from './__tests__/__mocks__/handlers';
+import "@testing-library/jest-dom";
+import "jest-localstorage-mock";
+import { server } from "./__tests__/test-utils";
+import { Response, Request, Headers } from "node-fetch";
 
-const server = setupServer(...handlers);
+// Make Response, Request, and Headers globally available
+global.Response = Response;
+global.Request = Request;
+global.Headers = Headers;
 
-// Start the server before all tests
-beforeAll(() => server.listen());
+// Set up environment variables for tests
+process.env.SUPABASE_URL = "https://test-supabase-url.com";
+process.env.SUPABASE_ANON_KEY = "test-anon-key";
 
-// Reset handlers after each test
-afterEach(() => server.resetHandlers());
+// Start MSW server before all tests
+beforeAll(() => {
+  console.log("Starting MSW server");
+  server.listen({ onUnhandledRequest: 'warn' });
+});
+
+// Reset handlers after each test for isolation
+afterEach(() => {
+  console.log("Resetting MSW handlers");
+  server.resetHandlers();
+  localStorage.clear();
+  jest.clearAllMocks();
+});
 
 // Clean up after all tests
-afterAll(() => server.close());
+afterAll(() => {
+  console.log("Closing MSW server");
+  server.close();
+});
+
+// Mock window.matchMedia which is not implemented in JSDOM
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+// Mock ResizeObserver which is not implemented in JSDOM
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
